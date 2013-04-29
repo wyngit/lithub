ab1e84daefadebc2d304f23739e25baf62cdfc25
Hannah Krier <hannahkrier@berkeley.edu>
2013-04-28 22:30:44 -0700

    Made basic layout; inserted comments; created a list of all comments

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
+   
    $.getJSON('/listcomments', function(data) {
-       console.log(data);
+       comments = data;
+       $(comments).each(function () {
+			if (authors.indexOf(this.user.login) == -1) {
+				authors.push(this.user.login);
+			}
+		});
+		setUpAuthors();
+		insertComments();
    });
 });
+
+var comments = {};
+var authors = [];
+
+
+function setUpAuthors() {
+	$(authors).each(function () {
+		$('#comments').append('<input type="radio" value="' + this + '">' + this + '</input>');
+	});
+}
+
+function insertComments() {
+	var oldHtml = $('#text-container').html();
+	$(comments).each(function () {
+		var commentHtml = '<span class="red">' + this.body + '</span>';
+		if (this.position === null) {
+			oldHtml += commentHtml;
+		} else {
+			oldHtml = insertAtPosition(oldHtml, commentHtml, this.position);
+		}
+	});
+	$('#text-container').html(oldHtml);
+}
+
+function insertAtPosition(oldString, insertString, position) {
+	return [oldString.slice(0, position), insertString, oldString.slice(position)].join('');
+}
