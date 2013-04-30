7dd0ff18e85a818d439056c9f2e812b99c366e95
Hannah Krier <hannahkrier@berkeley.edu>
2013-04-29 23:35:11 -0700

    Created comment toggling

--- static/js/main.js
+++ static/js/main.js
@@ -3,38 +3,53 @@ $(document).ready(function() {
    $.getJSON('/listcomments', function(data) {
        comments = data;
        $(comments).each(function () {
-			if (authors.indexOf(this.user.login) == -1) {
-				authors.push(this.user.login);
+			if (authors[this.user.login] == undefined) {
+				authors[this.user.login] = true;
+				authorColors[this.user.login] = colors.pop();
 			}
 		});
 		setUpAuthors();
-		insertComments();
+		insertComments(authors);
    });
+   
 });
 
 var comments = {};
-var authors = [];
+var authors = {};
+var authorColors = {};
+// right now only 6 colors (so up to 6 authors allowed)
+var colors = ['red', 'blue', 'green', 'yellow', 'magenta', 'cyan']; 
+var OLD_HTML = $('#text-container').html();
 
 
 function setUpAuthors() {
-	$(authors).each(function () {
-		$('#comments').append('<input type="radio" value="' + this + '">' + this + '</input>');
+	$.each(authors, function (key, value) {
+		var authorString = '<input type="checkbox" name="authors" checked="' + value + '" value="' + key + '">' + key + '</input>'
+		$('#comments').append(authorString);
+	});
+	
+	$('input:checkbox').change(function() {
+   		authors[this.value] = this.checked;
+   		insertComments(authors);
 	});
 }
 
-function insertComments() {
-	var oldHtml = $('#text-container').html();
+function insertComments(authorsList) {
+	var newHtml = OLD_HTML;
 	$(comments).each(function () {
-		var commentHtml = '<span class="red">' + this.body + '</span>';
-		if (this.position === null) {
-			oldHtml += commentHtml;
-		} else {
-			oldHtml = insertAtPosition(oldHtml, commentHtml, this.position);
+		if (authors[this.user.login]) {
+			var commentHtml = '<span class="' + authorColors[this.user.login] +'">' + this.body + '</span>';
+			if (this.position === null) {
+				newHtml += commentHtml;
+			} else {
+				newHtml = insertAtPosition(newHtml, commentHtml, this.position);
+			}
 		}
 	});
-	$('#text-container').html(oldHtml);
+	$('#text-container').html(newHtml);
 }
 
 function insertAtPosition(oldString, insertString, position) {
 	return [oldString.slice(0, position), insertString, oldString.slice(position)].join('');
 }
+
